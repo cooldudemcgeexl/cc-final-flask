d8cbc297202ec185408563177bf810b84da87323
Nick Lay <layns@mail.uc.edu>
2023-04-19 19:33:37 -0400

    update household to be a dataclass for serialization

--- final/models/household.py
+++ final/models/household.py
@@ -1,11 +1,21 @@
+from dataclasses import dataclass
+
 from sqlalchemy import Boolean, Column, Integer, String
 
 from ..app.database import db
 
 
+@dataclass
 class Household(db.Model):
     __tablename__ = 'households'
-    
+    hshd_num: int
+    loyalty_flag: bool
+    age_range: str
+    marital_status: str
+    income_range: str
+    hshd_size: int
+    children: int
+
     hshd_num = Column(Integer, primary_key=True, nullable=False)
     loyalty_flag = Column(Boolean, nullable=False)
     age_range = Column(String(5))
@@ -13,3 +23,4 @@ class Household(db.Model):
     income_range = Column(String(30))
     hshd_size = Column(Integer)
     children = Column(Integer)
+
