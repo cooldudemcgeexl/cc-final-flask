c320ae5154427be6738e6549962b4b6f09f05a05
Nick Lay <layns@mail.uc.edu>
2023-04-20 22:35:09 -0400

    build searchable datatable

--- final/blueprints/data.py
+++ final/blueprints/data.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from ..app.database import db
@@ -10,5 +10,14 @@ data = Blueprint('data', __name__, url_prefix='/data')
 
 @data.route('/pull')
 def data_pull():
-    data_pull: DataPull = DataPull.query.filter_by(hshd_num=10).all()
-    return render_template('data_table.html', data_pull=data_pull)
+    household_num = request.args.get('household_num')
+    data_pull = None
+
+    if household_num:
+        data_pull: DataPull = DataPull.query.filter_by(hshd_num=household_num).all()
+    return render_template('data_table.html', data_pull=data_pull, household_num=household_num)
+
+
+@data.route('/sample')
+def data_pull_sample():
+    return redirect(f"{url_for('data.data_pull')}?household_num=10")
