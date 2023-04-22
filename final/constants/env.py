3f52a9de69eb187e2ad5a598faddd6dec547cee9
Nick Lay <layns@mail.uc.edu>
2023-04-21 22:14:39 -0400

    add ability to switch between test and """"Prod"""" db

--- final/constants/env.py
+++ final/constants/env.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-ENV_PATH = Path(__file__).parents[1] / '.env'
+ENV_PATH = Path(__file__).parents[1] / ".env"
 load_dotenv(ENV_PATH.absolute())
 
-POSTGRES_PWD = os.getenv('POSTGRES_PWD')
+POSTGRES_PWD = os.getenv("POSTGRES_PWD")
+DB_TYPE = os.getenv("DB_TYPE")
+IS_TEST_DB = DB_TYPE.lower() == "test"
--- final/constants/postgres.py
+++ final/constants/postgres.py
@@ -1,5 +1,5 @@
-from .env import POSTGRES_PWD
+from .env import IS_TEST_DB, POSTGRES_PWD
 
-POSTGRES_DB_NAME = 'cloud-comp-final'
+POSTGRES_DB_NAME = "cloud-comp-test" if IS_TEST_DB else "cloud-comp-final"
 
-POSTGRES_CON_STR = f'postgresql://cc_postgres_admin:{POSTGRES_PWD}@cc-final-postgres2.postgres.database.azure.com/{POSTGRES_DB_NAME}?sslmode=require'
+POSTGRES_CON_STR = f"postgresql://cc_postgres_admin:{POSTGRES_PWD}@cc-final-postgres2.postgres.database.azure.com/{POSTGRES_DB_NAME}?sslmode=require"
